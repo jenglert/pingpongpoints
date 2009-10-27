92a15d2ca9ba9a30e07cf717f79caec93b8f9926
James Englert <englert.james@gmail.com>
2009-10-27 19:16:12 -0400

    Updating send mail.

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,15 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  
+  config.action_mailer.smtp_settings = {
+    :address        => 'mail.thepeoplesfeed.com',
+    :port           => 26,
+    :domain         => 'www.thepeoplesfeed.com',
+    :authentication => :login,
+    :user_name      => 'webmaster@thepeoplesfeed.com',
+    :password       => 'thepeoplesfeed'
+  }
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
