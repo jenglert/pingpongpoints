257cc43fe59d7705cb175017fa453ca9b4088077
James Englert <englert.james@gmail.com>
2009-11-07 19:50:58 -0500

    Adding a match observer so that we will now twitter the matches as they occur.

--- app/models/match_observer.rb
+++ app/models/match_observer.rb
@@ -1,5 +1,16 @@
+require 'twitter'
+
 class MatchObserver < ActiveRecord::Observer
   def after_create(match)
     rating_change = EloSystem.calculate_rankings match 
+    
+    tweet_match match
+    
+  end
+  
+  def tweet_match match
+    httpauth = Twitter::HTTPAuth.new('pingpongpoints', 'zepher')
+    client = Twitter::Base.new(httpauth)
+    client.update("#{match.winner.login} #{ApplicationController.helpers.defeated_synonym} #{match.loser.login} #{match.winner.score}-#{match.loser.score}")
   end
 end
