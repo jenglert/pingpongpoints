ffd261566f927fb215b0d22a25661fd5b7ae7d05
James Englert <englert.james@gmail.com>
2009-11-10 15:20:31 -0500

    Fixing the ratings

--- lib/elo_system.rb
+++ lib/elo_system.rb
@@ -3,11 +3,11 @@ module EloSystem
   include Math
 
   # K refers to the K rating of matches.  This number controls how quickly ratings will change.
-  K = 64
+  K = 64.0
   
   # This is the number of points that one player will need in order to have a "major victory" over the other player.
   # A major victory will generate a larger swing in rating.
-  MAJOR_DEFEAT_MARGIN = 5
+  MAJOR_DEFEAT_MARGIN = 5.0
   
   def recalculate_all_rankings
     # Reset all data to the original state
@@ -19,9 +19,7 @@ module EloSystem
   
   def calculate_rankings(match)
     
-    chance_higher_rated_player_will_win = calculate_chance_to_win match.rating_difference
-
-    rating_change = calculate_rating_change(match.higher_rated_participant_adj_score,chance_higher_rated_player_will_win, match.winner.score - match.loser.score > MAJOR_DEFEAT_MARGIN)
+    rating_change = calculate_rating_change(match.higher_rated_participant_adj_score,match.rating_difference, match.winner.score - match.loser.score > MAJOR_DEFEAT_MARGIN)
     
     match.winner.update_attribute :rating, match.winner.rating + rating_change
     match.loser.update_attribute :rating, match.loser.rating - rating_change
@@ -37,6 +35,7 @@ module EloSystem
       margin_of_victory_mulitiplier = 1.5
     end
     
+    puts rating_difference
     chance_higher_rated_player_will_win = calculate_chance_to_win rating_difference
     
     (margin_of_victory_mulitiplier * K * (did_higher_rated_player_adj_score - chance_higher_rated_player_will_win)).round.abs
@@ -45,9 +44,9 @@ module EloSystem
   # Calculates the % chance that the higher rated player will win based on the rating difference.
   def calculate_chance_to_win(rating_difference)
     if rating_difference > 0
-      1 - 1/(E ** (rating_difference/200.0) + 1)
+      1 - 1/(E ** (rating_difference/200.0) + 1.0)
     else
-       1/(E ** (rating_difference.abs/200.0) + 1)
+       1/(E ** (rating_difference.abs/200.0) + 1.0)
     end
   end
   
